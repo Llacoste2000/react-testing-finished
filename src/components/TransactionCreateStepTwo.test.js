43645e3e8a21f84cb0c1e8ba024f7eb883950b75
Louis LACOSTE <louis@lm-agence.fr>
2021-09-24 16:28:29 +0200

    feat(doc): change

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,14 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
 
-test('if an amount and note is entered, the pay button becomes enabled', async () => {
-    render(<TransactionCreateStepTwo sender={{ id: '5' }} receiver={{ id: '5' }} />);
+const mockCreateTransaction = jest.fn();
+const mockShowSnackbar = jest.fn();
 
-    expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled();
+test("if an amount and note is entered, the pay button becomes enabled", async () => {
+  render(
+    <TransactionCreateStepTwo
+      sender={{ id: "5" }}
+      receiver={{ id: "5" }}
+      createTransaction={mockCreateTransaction}
+      showSnackbar={mockShowSnackbar}
+    />
+  );
 
-    userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
-    userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
 
-    expect(await screen.findByRole('button', { name: /pay/i })).toBeEnabled();
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+  userEvent.click(screen.getByRole("button", { name: /pay/i }));
+  await act(() => Promise.resolve());
+  expect(mockCreateTransaction).toHaveBeenCalled();
+  userEvent.click(await screen.findByRole("button", { name: /request/i }));
 });
